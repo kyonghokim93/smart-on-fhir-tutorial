2efee42f4c6409a802ca70c3c7ad17d4a74ce17e
Kyong Ho Kim <97008074+kyonghokim93@users.noreply.github.com>
2023-02-11 11:37:33 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -60,7 +60,7 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
-          p.temperature = getQuantityValueAndUnit(temperature[0]);
+          p.temperature = getQuantityValueAndUnit(temperature);
 
           ret.resolve(p);
         });
