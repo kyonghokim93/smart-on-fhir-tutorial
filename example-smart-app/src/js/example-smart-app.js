b429aecacfb43e1d5edb1f74fd5251f330225430
Kyong Ho Kim <97008074+kyonghokim93@users.noreply.github.com>
2023-02-11 17:25:31 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,6 +25,7 @@
                   });
 
         $.when(pt, obv).fail(onError);
+        
 
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
